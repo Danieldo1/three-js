f32f0911e5d0765fd29133556ac944e624bf60ba
Dan <daniel.speranskiy@live.com>
2023-11-20 11:06:05 +0400

    Adding in the homepage basic styling using framer motion

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,12 @@
 import { proxy } from "valtio";
 
 const store = proxy({
-    
+    intro: true,
+    color: '#8E9BBB',
+    isLogoTexture: true,
+    isFullTexture: false,
+    logoDecal: './threejs.png',
+    fullDecal: './threejs.png',
 })
 
 export default store
